9152d5619c23241d5eaf3840388f6585ec2a2d35
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-05 08:56:22 -0300

    feat: keeping the stylesheet only where it is necessary

--- tests/main/bat-pass-app/src/screens/Home.tsx
+++ tests/main/bat-pass-app/src/screens/Home.tsx
@@ -1,17 +1,8 @@
 import React from "react";
-import {StyleSheet, Text} from "react-native";
+import {Text} from "react-native";
 
 export default function Home() {
     return (
         <Text>Home works!!</Text>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-});
