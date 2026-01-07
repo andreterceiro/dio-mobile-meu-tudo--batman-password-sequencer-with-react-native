574073825568913c5518e28ca414d52669a4d298
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-07 08:18:46 -0300

    docs: reoving the use of the example component 'Menu'

--- tests/main/bat-pass-app/src/screens/Home.tsx
+++ tests/main/bat-pass-app/src/screens/Home.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import {View, Text} from "react-native";
-import { Menu } from "../components/Menu/Menu";
 
 export default function Home() {
     return (
           <View>
-            <Menu></Menu>
             <Text>Home works!!</Text>
           </View>
     );
