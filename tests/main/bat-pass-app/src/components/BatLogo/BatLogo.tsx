4039d6945eb94a6a9b225e7bef8b223cf32b8b7d
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-07 08:25:40 -0300

    feat: main file of the new component BatLogo - initital code made using the extension R Component - the previous commit related to this subject was wrong - and adjusting the path to loas the stylesheet

---
+++ tests/main/bat-pass-app/src/components/BatLogo/BatLogo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './BatLogoStyles';
+
+export function BatLogo() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
