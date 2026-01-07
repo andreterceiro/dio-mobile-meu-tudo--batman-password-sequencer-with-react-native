e9c36be0846470948cd9171cb583ef1781e8c06e
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-07 08:28:07 -0300

    feat: insertig a text in the new component

--- tests/main/bat-pass-app/src/components/BatLogo/BatLogo.tsx
+++ tests/main/bat-pass-app/src/components/BatLogo/BatLogo.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { styles } from './BatLogoStyles';
 
 export function BatLogo() {
   return (
     <View style={styles.container}>
-
+      <Text>Bat pass generator</Text>
     </View>
   );
 }
