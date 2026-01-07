ee49328a3f57553274e7cd90c52a810b2a31ce7e
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-07 09:15:03 -0300

    feat: inserting the BatLogo component in Home.tsx

--- tests/main/bat-pass-app/src/screens/Home.tsx
+++ tests/main/bat-pass-app/src/screens/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {View, Text} from "react-native";
+import {BatLogo} from '../components/BatLogo/BatLogo'
 
 export default function Home() {
     return (
           <View>
+            <BatLogo />
             <Text>Home works!!</Text>
           </View>
     );
