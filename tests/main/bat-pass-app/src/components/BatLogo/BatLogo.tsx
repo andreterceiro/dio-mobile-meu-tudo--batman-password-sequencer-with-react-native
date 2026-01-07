9f5468b1e399ba7a49db6e23fd604bc91efaea15
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-07 09:14:36 -0300

    feat: inserting the Batman logo in the BatLogo component

--- tests/main/bat-pass-app/src/components/BatLogo/BatLogo.tsx
+++ tests/main/bat-pass-app/src/components/BatLogo/BatLogo.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { View, Text, Image } from 'react-native';
+import batLogo from "../../../assets/bat-logo.png"
 import { styles } from './BatLogoStyles';
 
 export function BatLogo() {
   return (
     <View style={styles.container}>
       <Text>Bat pass generator</Text>
+      <Image
+        source={batLogo}
+      />
     </View>
   );
 }
