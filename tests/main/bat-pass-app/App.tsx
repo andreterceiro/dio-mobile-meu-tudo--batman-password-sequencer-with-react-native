6135715a61efcd726da039a2dcee26a0c6a7cda2
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-08 09:58:47 -0300

    feat: now App.tsx only has the <Home /> component

--- tests/main/bat-pass-app/App.tsx
+++ tests/main/bat-pass-app/App.tsx
@@ -1,14 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import Home from "./src/screens/Home";
-import styles from "./src/screens/Style"
 
 export default function App() {
   return (
-    <View>
-      <Home />
-      <Text>Coringão</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
