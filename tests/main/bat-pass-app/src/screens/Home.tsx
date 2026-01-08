c9dd133aa76f561be593bd354826deae16df5839
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-08 08:38:18 -0300

    feat: using 'logoContainer' stylesheet property

--- tests/main/bat-pass-app/src/screens/Home.tsx
+++ tests/main/bat-pass-app/src/screens/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {View, Text} from "react-native";
 import {BatLogo} from '../components/BatLogo/BatLogo'
+import styles from "./Style"
 
 export default function Home() {
     return (
           <View>
-            <View>
+            <View style={styles.logoContainer}>
               <BatLogo />
             </View>
           </View>
