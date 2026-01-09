365e58d43ca9bf6f04c0752029302bb0c7a0b519
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-09 09:03:56 -0300

    feat: instead of return an external view, now we return a text input as external component

--- tests/main/bat-pass-app/src/components/BatTextInput/BatTextnput.tsx
+++ tests/main/bat-pass-app/src/components/BatTextInput/BatTextnput.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { View, TextInput } from 'react-native';
+import { TextInput } from 'react-native';
 
 export function BatTextInput() {
   return (
-    <View>
-        <TextInput placeholder="pass"></TextInput>
-    </View>
+      <TextInput placeholder="pass">
+      </TextInput>
   );
 }
