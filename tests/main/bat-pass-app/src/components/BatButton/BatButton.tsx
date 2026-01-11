1a15538ed869f93ff7ab93b6c60262d78c63e47e
André de Paula Terceiro <andre@terceiro.com.br>
2026-01-11 16:20:53 -0300

    feat: inserting a button in BatButton main file

--- tests/main/bat-pass-app/src/components/BatButton/BatButton.tsx
+++ tests/main/bat-pass-app/src/components/BatButton/BatButton.tsx
@@ -6,7 +6,11 @@ import { styles } from './BatButtonStyles';
 export function BatButton() {
   return (
     <View style={styles.container}>
-
+        <Button
+            onPress={() => console.log('olá')}
+            title="Clique aqui"
+            accessibilityLabel='clique em mim'
+        />
     </View>
   );
 }
