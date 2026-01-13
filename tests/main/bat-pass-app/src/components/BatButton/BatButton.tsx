7e9b0b1b39bdaded915620e5ac921f11508ecd8b
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-13 09:14:32 -0300

    feat: updating the value of the <BatTextInput /> through a property using useState()

--- tests/main/bat-pass-app/src/components/BatButton/BatButton.tsx
+++ tests/main/bat-pass-app/src/components/BatButton/BatButton.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, View, Text, Pressable } from 'react-native';
 import {BatTextInput} from '../BatTextInput/BatTextnput';
 
 import { styles } from './BatButtonStyles';
 
 export function BatButton() {
+  const [pass, setPass] = useState("");
+
+  function handleGenerateButton() {
+    setPass("olá");
+  }
+
   return (
     <>
-        <BatTextInput />
-        <Pressable style={styles.button} onPress={() => console.log("Generate")}>
+        <BatTextInput pass={pass} />
+        <Pressable style={styles.button} onPress={handleGenerateButton}>
           <Text style={styles.text}>Generate</Text>
         </Pressable>
         <Pressable style={styles.button} onPress={() => console.log("Copy")}>
