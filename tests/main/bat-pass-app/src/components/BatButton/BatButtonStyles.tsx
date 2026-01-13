9f5afaf7200e1578ef13a933b020a7e51a068111
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-13 08:31:47 -0300

    feat: formatting the style of the pressable area text

--- tests/main/bat-pass-app/src/components/BatButton/BatButtonStyles.tsx
+++ tests/main/bat-pass-app/src/components/BatButton/BatButtonStyles.tsx
@@ -2,5 +2,11 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {},
-  text: {}
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: '#E5BF3C'
+  }
 });
