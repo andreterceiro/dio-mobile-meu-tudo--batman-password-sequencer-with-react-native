0f88d66c5bdfc6349490fcdfd1d3bbb8402734d8
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-15 13:26:51 -0300

    feat: creating the code related to the button 'copy'

--- tests/main/bat-pass-app/src/components/BatButton/BatButton.tsx
+++ tests/main/bat-pass-app/src/components/BatButton/BatButton.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { Text, Pressable } from 'react-native';
 import {BatTextInput} from '../BatTextInput/BatTextInput';
-
+import * as Clipboard from 'expo-clipboard';
 import { styles } from './BatButtonStyles';
 import generatePass from '../../services/passwordService';
 
@@ -14,13 +14,17 @@ export function BatButton() {
     setPass(generateToken);
   }
 
+  async function handleCopyButton() {
+    await Clipboard.setStringAsync(pass)
+  }
+
   return (
     <>
         <BatTextInput pass={pass} />
         <Pressable style={styles.button} onPress={handleGenerateButton}>
           <Text style={styles.text}>Generate</Text>
         </Pressable>
-        <Pressable style={styles.button} onPress={() => console.log("Copy")}>
+        <Pressable style={styles.button} onPress={handleCopyButton}>
           <Text style={styles.text}>Copy </Text>
         </Pressable>  
     </>
